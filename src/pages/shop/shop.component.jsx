fc623ff8970e9aaa6e445c0d2fadd4f990e46045
Manuel Escobar <mescobar@pragawebstudio.com>
2022-01-31 15:48:29 +0100

    update shope page, collection preview and collection item components

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import SHOP_DATA from "./shop.data";
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+class ShopPage extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    componentDidMount(){
+
+    }
+
+    render(){
+        const {collections} = this.state;
+        return (
+            <div className="shop-page">
+             <h1>SHOP PAGE</h1>
+             {collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+             ))}
+           
+            </div>
+        )
+    }
+}
+
+export default ShopPage;
