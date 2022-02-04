6a35cd0433c74427b2d04a3aaca20d71699cc44e
Manuel Escobar <mescobar@pragawebstudio.com>
2022-02-04 17:32:11 +0100

    created cart dropdown item component and redurer

--- src/components/cart-dropdown-item/cart-dropdown-item.component.jsx
+++ src/components/cart-dropdown-item/cart-dropdown-item.component.jsx
@@ -1,2 +1,16 @@
 import React from "react";
-import './cart-dropdown-item.styles.scss'
+import "./cart-dropdown-item.styles.scss";
+
+const CartDropdownItem = ({ item: { imageUrl, name, price, quantity } }) => (
+  <div className="cart-dropdown-item">
+    <img className="image" alt="item" src={imageUrl} />
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span className="price">
+        {quantity} x ${price}
+      </span>
+    </div>
+  </div>
+);
+
+export default CartDropdownItem;
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import './cart-dropdown.styles.scss';
+import { connect } from "react-redux";
+import "./cart-dropdown.styles.scss";
 import CustomButton from "../custom-button/custom-button.component";
+import CartDropdownItem from "../cart-dropdown-item/cart-dropdown-item.component";
 
-const CartDropdown = () => (
-    <div className="cart-dropdown">
-        <div className="cart-items">
-        </div>
-        <CustomButton >GO TO CHECKOUT</CustomButton>
+const CartDropdown = ({ cartItems }) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.map(cartItem => 
+        <CartDropdownItem key={cartItem.id} item={cartItem} />
+      )}
     </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
 );
 
-export default CartDropdown
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
