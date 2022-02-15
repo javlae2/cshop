0b30e12f267a4be0c20e05dfbfd0102a1a69a188
Manuel Escobar <mescobar@pragawebstudio.com>
2022-02-15 15:27:27 +0100

    added styles and selectors for collection page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import CollectionItem from "../collection-item/collection-item.component";
 import './collection-preview.style.scss';
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({title, items, limit = 4}) => (
     <div className="collection-preview">
         <h2 className="title">{title.toUpperCase()}</h2>
         <div className="preview">
             {items
-                .filter((item,idx) => idx < 4)
+                .filter((item,idx) => idx < limit)
                 .map(( item ) => (
                     <CollectionItem key={item.id} item={item}/>
             ))}
